36f08bb7de7abce4f81437615eaa2bcf21ab7b2d
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-05-15 18:09:02 +0500

    .

--- 03-javascript/4 - control-flow/main.js
+++ 03-javascript/4 - control-flow/main.js
@@ -1,14 +1,47 @@
-// 1. Voyaga yetgan va yetmaganligini aniqlash
-let yosh = 12
-if (yosh > 17) {
-    console.log("Siz voyaga yetgansiz")
+
+//^ 1. Voyaga yetgan va yetmaganligini aniqlash
+// let yosh = 12
+// if (yosh > 17) {
+//     console.log("Siz voyaga yetgansiz")
+// } else {
+//     console.log("Siz voyaga yetmagansiz")
+// }
+//^ 2. Kiritilgan son juft yoki toqligini aniqlasin.
+// let son = 132
+// if (son % 2 === 0) {
+//     console.log("Siz juft son kiritdingiz")
+// }  else {
+//     console.log("Siz toq son kiritdingiz")
+// }
+//^ 3. O'quvchi sinfini kiritsin, agarda 11sonidan katta son kiritsa siz maktabni tugatdingiz aks holda siz hali o'quvchisiz javobini chiqaring
+// let sinf = 3
+// if (sinf > 11) {
+//     console.log("Siz maktabni tugatgansiz")
+// } else {
+//     console.log("Siz hali o'quvchisiz")
+// }
+
+//^ 4. Foydalanuvchi 30 va undan katta son kiritganda bugun havo juda issiq aks holda bugun havo yaxshi javobi ekranga chiqarilsin
+// let daraja = 40
+// if (daraja < 30) {
+//     console.log("Bugun havo yaxshi")
+// } else {
+//     console.log("Bugun havo vapshe issiq")
+// }
+
+//^ 5. Foydalanuvchi kiritgan son musbat va manfiy ekanligini aniqlansin, agarda 0 kiritsa siz 0 sonini kiritdingiz javobi ekranga chiqarilsin
+let son = -1
+if (son > 0) {
+    console.log("Siz musbat son kiritdingiz")
+} else if (son === 0) {
+    console.log("Siz nol sonini kiritdingiz")
+} else (
+    console.log("Siz manfiy son kiritingiz")
+)
+//^ 6. Foydalanuvchi "qweqwe" parolni kiritsa tizim ochildi aks holda noto'g'ri parol javobi ekranga chiqarilsin
+let parol = "qweqwe"
+if (parol === "qweqwe") {
+    console.log("Tizim ochildi")
 } else {
-    console.log("Siz voyaga yetmagansiz")
-}
-// 2. Kiritilgan son juft yoki toqligini aniqlasin.
-let son = 132
-if (son % 2 === 0) {
-    console.log("Siz juft son kiritdingiz")
-}  else {
-    console.log("Siz toq son kiritdingiz")
+    console.log("Parol xato")
 }
