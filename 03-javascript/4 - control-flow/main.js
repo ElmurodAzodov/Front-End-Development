d3be013f6b89133fd1c2e58cd2913327be5e2b79
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-05-15 16:56:15 +0500

    juft va toqni aniqlash

--- 03-javascript/4 - control-flow/main.js
+++ 03-javascript/4 - control-flow/main.js
@@ -5,3 +5,10 @@ if (yosh > 17) {
 } else {
     console.log("Siz voyaga yetmagansiz")
 }
+// 2. Kiritilgan son juft yoki toqligini aniqlasin.
+let son = 132
+if (son % 2 === 0) {
+    console.log("Siz juft son kiritdingiz")
+}  else {
+    console.log("Siz toq son kiritdingiz")
+}
