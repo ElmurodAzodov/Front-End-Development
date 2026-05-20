ea30d83fd1c2a45a14c075bf03055763e10cafca
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-05-20 11:55:52 +0500

    add

---
+++ 03-javascript/2 - variables-and-data-types/main.js
@@ -0,0 +1,20 @@
+
+// let a = 17
+// a = 19
+// console.log(a)
+
+// let a = 17
+// let a = 19 // SyntaxError
+// console.log(a)
+
+
+// console.log(son) // ReferenceError
+// let son = 15
+
+// console.log(son1) // undefined
+// var son1 = 13
+
+// var son = 12
+// son = 13
+// console.log(son)
+
