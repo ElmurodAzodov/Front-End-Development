f94dbe2feeaa65e01bd29f687f7f092cf79750ed
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-05-20 12:29:08 +0500

    add

--- 03-javascript/3 - operators/main.js
+++ 03-javascript/3 - operators/main.js
@@ -1,3 +1,22 @@
+// console.log(2 + 2)
+// console.log("2" + 2)
 
-console.log(2 + 2)
-console.log("2" + 2)
+// let a = 12
+// let b = "4"
+// console.log(a + b, typeof(a + b))
+// console.log(a - b, typeof(a - b))
+// console.log(a * b, typeof(a * b))
+// console.log(a / b, typeof(a / b))
+
+let a = "12";
+let b = 12;
+
+// console.log( typeof(a), a == b)
+// console.log( typeof(b), a === b)
+
+console.log(typeof a, typeof b, a > b);
+console.log(typeof a, typeof b, a < b);
+console.log(typeof a, typeof b, a != b);
+console.log(typeof a, typeof b, a !== b);
+
+console.log("string" === "number");
