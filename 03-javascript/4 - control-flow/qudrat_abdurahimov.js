9fcc1d9cef4710062488f3725a170052dfa41ef5
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-05-21 16:01:20 +0500

    add

--- 03-javascript/4 - control-flow/qudrat_abdurahimov.js
+++ 03-javascript/4 - control-flow/qudrat_abdurahimov.js
@@ -1,11 +1,19 @@
 //* Shart operatoriga oid masalalar
 //^ if1
-let son = 2
-if (son > 0) {
-    console.log(son + 1)
-} else {
-    console.log(son)
-}
+// let son = 2
+// if (son > 0) {
+//     console.log(son + 1)
+// } else {
+//     console.log(son)
+// }
 
-//^ if2
-
+//^ if3
+// let son = 0
+// if (son > 0) {
+//     console.log(son + 1)
+// } else if (son < 0) {
+//     console.log(son - 2)
+// } else {
+//     son = 10
+//     console.log(son)
+// }
