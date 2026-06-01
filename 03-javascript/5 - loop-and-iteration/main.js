2e789842d5264a1404ef7c3d0450af1d4721a002
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-01 17:57:19 +0500

    add

---
+++ 03-javascript/5 - loop-and-iteration/main.js
@@ -0,0 +1,25 @@
+
+// let i = 0
+// console.log(i++)
+// console.log(i++)
+// console.log(i)
+// console.log(++i)
+
+// for (let boshlanishi = 1; boshlanishi <= 100; boshlanishi+1 ) {
+//     console.log(boshlanishi)
+// }
+
+//* Juft sonlari
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+//* 0dan 100gacha oraliqdagi faqat 5ga bolinadigan sonlar
+for (let i = 0; i <= 100; i++ ) {
+    console.log(i)
+}
+//* 0dan 100gacha oraliqdagi faqat 5ga va 3ga bolinadigan sonlar
+//* 0dan 100gacha oraliqdagi faqat 3ga va 4ga bolinadigan sonlar
