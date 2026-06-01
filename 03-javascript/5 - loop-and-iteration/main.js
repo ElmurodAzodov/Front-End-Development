4d90b8cc63458d68e25e59c2041b3d747a0edf2f
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-01 23:25:52 +0500

    .

--- 03-javascript/5 - loop-and-iteration/main.js
+++ 03-javascript/5 - loop-and-iteration/main.js
@@ -10,25 +10,25 @@
 
 //* Juft sonlari
 
-for (let i = 0; i <= 100; i++) {
-  if (i % 2 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
 
 //* 0dan 100gacha oraliqdagi faqat 5ga bolinadigan sonlar
-for (let i = 0; i <= 100; i++) {
-  console.log(i);
-}
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
 //* 0dan 100gacha oraliqdagi faqat 5ga va 3ga bolinadigan sonlar
-for (let i = 0; i <= 100; i++) {
-  if (i % 5 === 0 && i % 3 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 5 === 0 && i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
 //* 0dan 100gacha oraliqdagi faqat 3ga va 4ga bolinadigan sonlar
-for (let i = 0; i <= 100; i++) {
-  if (i % 3 === 0 && i % 4 === 0) {
-    console.log(i);
-  }
-}
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 4 === 0) {
+//     console.log(i);
+//   }
+// }
