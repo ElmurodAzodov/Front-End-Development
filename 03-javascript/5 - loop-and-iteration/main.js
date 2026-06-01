47af612c48d3ad64701b836b31f3c9ecb7394b59
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-01 23:23:58 +0500

    .

--- 03-javascript/5 - loop-and-iteration/main.js
+++ 03-javascript/5 - loop-and-iteration/main.js
@@ -22,4 +22,9 @@
 //     console.log(i)
 // }
 //* 0dan 100gacha oraliqdagi faqat 5ga va 3ga bolinadigan sonlar
+for (let i = 0; i <= 100; i++) {
+    if (i % 5 === 0 && i % 3 === 0) {
+        console.log(i)
+    }
+}
 //* 0dan 100gacha oraliqdagi faqat 3ga va 4ga bolinadigan sonlar
