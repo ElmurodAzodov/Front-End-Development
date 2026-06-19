71b00143e33360e4fca7010a0580c1b338a677e7
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-19 16:41:16 +0500

    array

---
+++ 03-javascript/6 - arrays/script.js
@@ -0,0 +1,16 @@
+
+let names = ["Saidjon", "Shohjahon", "Azamat", "Suhrob", "Jahongir"]
+
+// console.log(names)
+console.log(names[0])
+console.log(names[2])
+console.log(names[4])
+console.log(names[3])
+
+let massiv = new Array(4) // uzunligi 4ga teng bo'sh array
+let massiv0 = new Array(4,1,2,3,5)
+let massiv1 = []
+console.log(massiv)
+console.log(massiv0)
+console.log(massiv1)
+console.log(names.length)
