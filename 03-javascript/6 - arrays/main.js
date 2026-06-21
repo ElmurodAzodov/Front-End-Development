dd73a90b793d92b847c81ad0f77698c916f7f48b
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-21 23:42:14 +0500

    add

--- 03-javascript/6 - arrays/main.js
+++ 03-javascript/6 - arrays/main.js
@@ -29,15 +29,16 @@
 // 3 - Oxirgi elementni o'chirish
 // --------------------------------------------
 // [5, 10, 15, 20] massivining oxirgi elementini o'chiring va natijani chiqaring
-let arr = [5, 10, 15, 20]
-arr.pop()
-console.log(arr)
+// let arr = [5, 10, 15, 20]
+// arr.pop()
+// console.log(arr)
 
 // --------------------------------------------
 // 4 - Indeks bo'yicha olish
 // --------------------------------------------
 // ["olma", "anor", "banan"] massivining 2-indeksidagi elementni chiqaring
-
+let mevalar = ["olma", "anor", "banan"]
+console.log(mevalar[2])
 
 // --------------------------------------------
 // 5 - Massiv uzunligini chiqarish
