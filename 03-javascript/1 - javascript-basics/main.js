6c62d2f11cc676c93dec3ffe8801bd07a97b8e8d
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-22 16:18:47 +0500

    exam practice

---
+++ 03-javascript/1 - javascript-basics/main.js
@@ -0,0 +1,34 @@
+
+console.log("Boburjon")
+console.log('Boburjon')
+console.log("12", '23', 34, 45, "dssadsad")
+
+
+let ism = "Sherzodjon"
+ism = "Dastonjon"
+let yosh = 14
+let sinf = '5-sinf'
+let joy = "Urganch city"
+console.log(ism, yosh, sinf, joy)
+
+let son1 = 14
+let son2 = 6
+let natija = son1 + son2
+let natija1 = son1 - son2
+let natija2 = son1 * son2
+let natija3 = son1 / son2
+let natija4 = son1 % son2
+let natija5 = son1 ** son2
+console.log(natija)
+console.log(natija1)
+console.log(natija2)
+console.log(natija3)
+console.log(natija4)
+
+
+
+
+
+
+
+
