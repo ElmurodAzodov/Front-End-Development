7ed7ac0212fa90a6892f1e40c5bcbf18f35843ff
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-27 17:52:14 +0500

    add

--- 03-javascript/1 - javascript-basics/main.js
+++ 03-javascript/1 - javascript-basics/main.js
@@ -1,34 +1,55 @@
 
-console.log("Boburjon")
-console.log('Boburjon')
-console.log("12", '23', 34, 45, "dssadsad")
+// console.log("Boburjon")
+// console.log('Boburjon')
+// console.log("12", '23', 34, 45, "dssadsad")
 
 
-let ism = "Sherzodjon"
-ism = "Dastonjon"
-let yosh = 14
-let sinf = '5-sinf'
-let joy = "Urganch city"
-console.log(ism, yosh, sinf, joy)
+// let ism = "Sherzodjon"
+// const name = "Dastonbek"
+// ism = "Dastonjon"
+// let yosh = 14
+// let sinf = '5-sinf'
+// let joy = "Urganch city"
+// console.log(ism, yosh, sinf, joy)
 
-let son1 = 14
-let son2 = 6
-let natija = son1 + son2
-let natija1 = son1 - son2
-let natija2 = son1 * son2
-let natija3 = son1 / son2
-let natija4 = son1 % son2
-let natija5 = son1 ** son2
-console.log(natija)
-console.log(natija1)
+// let son1 = 14
+// let son2 = 6
+// let natija = son1 + son2
+// let natija1 = son1 - son2
+// let natija2 = son1 * son2
+// let natija3 = son1 / son2
+// let natija4 = son1 % son2
+// let natija5 = son1 ** son2
+// console.log(natija)
+// console.log(natija1)
+// console.log(natija2)
+// console.log(natija3)
+// console.log(natija4)
+
+
+//* Variables and Data Types
+
+// let son = 123
+// let ism = "Boburjon"
+// let natija1 = true
+// let natija2 = false
+// let qiymatsiz = undefined
+// let raqamEmas = NaN
+// let boshQiymat = null
+
+// console.log(son, typeof(son))
+// console.log(ism, typeof(ism))
+// console.log(natija1, typeof(natija1))
+// console.log(natija2, typeof(natija2))
+// console.log(qiymatsiz, typeof(qiymatsiz))
+// console.log(raqamEmas, typeof(raqamEmas))
+// console.log(boshQiymat, typeof(boshQiymat))
+
+
+let son = 12
+// let natija1 = son++ //12
+let natija2 = ++son //13
+// console.log(natija1)
 console.log(natija2)
-console.log(natija3)
-console.log(natija4)
-
-
-
-
-
-
 
 
