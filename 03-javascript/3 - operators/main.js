f83b57dc807193c72e3208031dfb381ede784996
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-30 23:40:06 +0500

    ADD

--- 03-javascript/3 - operators/main.js
+++ 03-javascript/3 - operators/main.js
@@ -46,4 +46,78 @@
 // console.log(typeof(num1))
 // console.log(typeof(num2))
 
-console.log()
+console.log("1)", 5 + 2, typeof (5 + 2));
+// 7 'number'
+
+console.log("2)", "5" + "2", typeof ("5" + "2"));
+// 52 'string'
+
+console.log("3)", "5" + 2, typeof ("5" + 2));
+// 52 'string'
+
+console.log("4)", "5" - 2, typeof ("5" - 2));
+// 3 'number'
+
+console.log("5)", 5 + "2", typeof (5 + "2"));
+// 52 'string'
+
+console.log("6)", 5 - "2", typeof (5 - "2"));
+// 3 'number'
+
+console.log("7)", 5 * 2, typeof (5 * 2));
+// 10 'number'
+
+console.log("8)", "5" * 2, typeof ("5" * 2));
+// 10 'number'
+
+console.log("9)", 5 * "2", typeof (5 * "2"));
+// 10 'number'
+
+console.log("10)", "5" / 2, typeof ("5" / 2));
+// 2.5 'number'
+
+console.log("11)", 5 / "2", typeof (5 / "2"));
+// 2.5 'number'
+
+console.log("12)", "5" % 2, typeof ("5" % 2));
+// 1 'number'
+
+console.log("13)", 5 % "2", typeof (5 % "2"));
+// 1 'number'
+
+console.log("14)", "5" + true, typeof ("5" + true));
+// 5true 'string'
+
+console.log("15)", "5" - true, typeof ("5" - true));
+// 4 'number'
+
+console.log("16)", true + true, typeof (true + true));
+// 2 'number'
+
+console.log("17)", false + false, typeof (false + false));
+// 0 'number'
+
+console.log("18)", null + 5, typeof (null + 5));
+// 5 'number'
+let son;
+console.log(son)
+console.log("19)", undefined + 5, typeof (undefined + 5)); // Not a Number - raqam
+// NaN 'number'
+
+console.log("20)", "hello" - 2, typeof ("hello" - 2));
+// NaN 'number'
+
+console.log("21)", "hello" * 2, typeof ("hello" * 2));
+// NaN 'number'
+
+console.log("22)", "5" + null, typeof ("5" + null));
+// 5null 'string'
+
+console.log("23)", "5" + undefined, typeof ("5" + undefined));
+// 5undefined 'string'
+
+console.log("24)", "" + 5, typeof ("" + 5));
+// 5 'string'
+
+console.log("25)", Number("5"), typeof Number("5"));
+// 5 'number'
