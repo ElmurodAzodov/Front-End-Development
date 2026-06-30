afbfeb223f8017afabe830159f7a0fbcc9c205e5
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-06-30 23:17:08 +0500

    add

--- 03-javascript/3 - operators/main.js
+++ 03-javascript/3 - operators/main.js
@@ -8,15 +8,42 @@
 // console.log(a * b, typeof(a * b))
 // console.log(a / b, typeof(a / b))
 
-let a = "12";
-let b = 12;
+// let a = "12";
+// let b = 12;
 
 // console.log( typeof(a), a == b)
 // console.log( typeof(b), a === b)
 
-console.log(typeof a, typeof b, a > b);
-console.log(typeof a, typeof b, a < b);
-console.log(typeof a, typeof b, a != b);
-console.log(typeof a, typeof b, a !== b);
+// console.log(typeof a, typeof b, a > b);
+// console.log(typeof a, typeof b, a < b);
+// console.log(typeof a, typeof b, a != b);
+// console.log(typeof a, typeof b, a !== b);
 
-console.log("string" === "number");
+// console.log("string" === "number");
+
+// let num1 = 100;
+// let num2 = "100";
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
+
+// console.log("Taqqoslash");
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+// console.log(num1 !== num2);
+// console.log(num1 != num2);
+// console.log(num1 = num2, typeof(num1 = num2));
+
+// console.log(typeof(num1))
+// console.log(typeof(num2))
+
+console.log()
