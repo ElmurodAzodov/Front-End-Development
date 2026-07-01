b4322b61da8d01956aaff9de67e84a9910971b24
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-01 17:59:04 +0500

    add

--- 03-javascript/6 - arrays/array_methods_practice.js
+++ 03-javascript/6 - arrays/array_methods_practice.js
@@ -14,7 +14,7 @@
 
 // 1-topshiriq (flat)
 // Ichma-ich joylashgan [1, [2, [3, [4, 5]]], 6] massivini to'liq (Infinity chuqurlikda) tekislang.
-
+console.log([1, [2, [3, [4, 5]]], 6].flat(Infinity))
 
 // 2-topshiriq (push)
 // Bo'sh "savat" massivi yarating. Foydalanuvchi tanlagan 3 ta mahsulot nomini (o'zingiz kiriting)
