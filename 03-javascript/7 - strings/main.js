99890a56ef2f34311acccee78956e9a0a9493eb8
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-06 16:40:06 +0500

    add

---
+++ 03-javascript/7 - strings/main.js
@@ -0,0 +1,20 @@
+
+// let text = "Salom mening ismim Shohruzbek"
+// let text = 'Salom mening ismim Mo"minjon'
+// console.log(text)
+
+// ============================================================
+
+let yosh = 13
+let ism = "Shohjahon"
+let soha = 'dasturchi'
+let malumoti = "Men oddiy insonman"
+console.log("Mening ismim ", ism,"va mening yoshim ", yosh, "da va men ", soha, "man!" )
+console.log(`Mening ismim ${ism} va mening yoshim ${yosh}da, keyin men ${soha}man!`)
+console.log(malumoti.length)
+
+let arr = ["olma", 'uzum', 1,2,3]
+arr[0] = 1
+console.log(arr)
+soha[0] = "i"
+console.log(soha)
