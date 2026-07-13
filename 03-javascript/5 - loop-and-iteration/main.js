ab3b8f6bc481fdd49e4a3b366d498f355b7fcf1a
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-13 17:25:29 +0500

    add

---
+++ 03-javascript/5 - loop-and-iteration/main.js
@@ -0,0 +1,17 @@
+// let yigindi = 0
+
+// for (let i = 0; i <= 100; i ++) {
+//     if (i % 2 !== 0) {
+//         yigindi = yigindi + i
+//     }
+    
+// }
+
+// console.log(yigindi)
+
+let son = parseInt(prompt("Son kiriting: "))
+let marta = parseInt(prompt("Siz kiritgan sonni nechi marta chiqarayin?: "))
+
+for (let i = 1; i <= marta; i++) {
+    console.log(`${i}-${son}`)
+}
---
+++ 03-javascript/8 - objects/main.js
@@ -0,0 +1,71 @@
+// let ism = "Shohjahon"
+// let yosh = 15
+// let malumoti = "o'rta"
+// let qiziqishlari = "ko'p uxlash va ko'p yatish"
+// let maqsad = "boy bo'lish"
+
+// let data = {
+//     ism: "Shohjahon",
+//     yosh: 15,
+//     malumoti: "o'rta",
+//     qiziqishlari: "ko'p uxlash va ko'p yatish",
+//     maqsad: "boy bo'lish"
+// }
+
+// console.log(ism, yosh, malumoti, qiziqishlari, maqsad)
+// console.log(data)
+
+//* ===========================================================================
+const shoxruzbek = {
+  ism: "Shoxruzbek",
+  familiya: "Karimov",
+  yosh: 8,
+  jins: "Erkak",
+  tugilganYil: 2018,
+  tugilganOy: "Mart",
+  tugilganKun: 15,
+  millati: "O'zbek",
+  fuqaroligi: "O'zbekiston",
+  shahri: "Toshkent",
+  tumani: "Yunusobod",
+  maktab: "12-maktab",
+  sinf: 2,
+  harfi: "A",
+  boyi: 130,
+  vazni: 28,
+  sochRangi: "Qora",
+  kozRangi: "Jigarrang",
+  sevimliFan: "Matematika",
+  sevimliRang: "Ko'k",
+  sevimliTaom: "Osh",
+  sevimliIchimlik: "Sharbat",
+  sevimliMeva: "Olma",
+  sevimliSport: "Futbol",
+  sevimliJamoa: "Real Madrid",
+  sevimliOyini: "Minecraft",
+  sevimliHayvon: "Sher",
+  sevimliKitob: "Zumrad va Qimmat",
+  sevimliMultfilm: "Tom va Jerry",
+  hobbi: "Rasm chizish",
+  orzusi: "Dasturchi bo'lish",
+  telefoniBor: false,
+  ukalariSoni: 1,
+  opalariSoni: 0,
+  dostlariSoni: 12,
+  yaxshiKoradiganFasl: "Yoz",
+  yaxshiKoradiganBayram: "Navro'z",
+  kompyuterBiladi: true,
+  inglizTiliniOrganmoqda: true,
+  baholari: [5, 5, 4, 5, 5],
+};
+shoxruzbek.lager = "Lagerga bormadi"
+
+console.log(Object.values(shoxruzbek))
+console.log(Object.keys(shoxruzbek))
+console.log(Object.entries(shoxruzbek))
+console.log(Object.entries(shoxruzbek)[40])
+
+// console.log(shoxruzbek);
+
+
+// console.log(shoxruzbek.familiya);
