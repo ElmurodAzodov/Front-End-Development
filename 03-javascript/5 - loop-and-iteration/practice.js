cae32d6b47ad4349a6e3e0bcf62772ab54d0d064
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-21 17:36:40 +0500

    add

--- 03-javascript/5 - loop-and-iteration/practice.js
+++ 03-javascript/5 - loop-and-iteration/practice.js
@@ -1,7 +1,7 @@
 // 1 dan 10 gacha bo‘lgan sonlarni chiqaring.
-// for (let i = 1; i <= 10; i++) {
-//   console.log(i);
-// }
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
 
 // 1 dan 20 gacha faqat juft sonlarni chiqaring (if va for yordamida).
 // for (let i = 1; i <= 20; i++) {
@@ -145,17 +145,17 @@
 // 5 ga bo‘linadiganlar o‘rniga "Buzz"
 // 3 va 5 ga bo‘linadiganlar o‘rniga "FizzBuzz" deb chiqaring.
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
-    console.log("Fizz");
-  } else if (i % 5 === 0) {
-    console.log("Buzz");
-  } else {
-    console.log(i);
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
 
 
 
