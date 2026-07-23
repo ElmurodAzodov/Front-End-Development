4f2e024782739890611931d9cde9954c1b5027f1
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-23 16:34:51 +0500

    add

--- 03-javascript/6 - arrays/array.js
+++ 03-javascript/6 - arrays/array.js
@@ -15,10 +15,28 @@
 
 let oquvchilar = ["Boburjon", "Marjona", "Abrorbek", "Hurmatbek", "Shohruzbek", "Dastonbek"]
 let natija = []
+let sonlar = [1,2,3,4,5,6,7,8,9,10]
 
+//& push()
 oquvchilar.push("Sherzodbek", "Axror")
 
-console.log(oquvchilar)
+//& pop()
+let saqlandi = oquvchilar.pop()
+
+//& unshift()
+oquvchilar.unshift(saqlandi)
+
+//& shift()
+let boshidanOchirdi = oquvchilar.shift()
+
+//& splice(start, deleteCount, ...items)
+sonlar.splice(1, 6, 0,0,0)
+
+console.log(sonlar)
+
+// console.log(oquvchilar)
+// console.log(saqlandi)
+// console.log(boshidanOchirdi)
 
 
 
