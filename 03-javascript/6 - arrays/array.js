d6d198a6e5782d4e3fbc447a3a84aa23f309797c
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-23 16:21:22 +0500

    add

--- 03-javascript/6 - arrays/array.js
+++ 03-javascript/6 - arrays/array.js
@@ -5,8 +5,47 @@
 // let tortinchiMeva = "banan"
 
 // console.log(birinchiMeva, ikkinchiMeva, uchinchiMeva, tortinchiMeva)
-let mevalar = ["olma", 1, 2, 3, 4, "anor", "uzum", "shaftoli", "apelsin", "mandarin"]
-console.log(mevalar)
-console.log(mevalar[5])
-// console.log(mevalar[-4])
-console.log(mevalar.length)
+// let mevalar = ["olma", 1, 2, 3, 4, "anor", "uzum", "shaftoli", "apelsin", "mandarin"]
+// console.log(mevalar)
+// console.log(mevalar[5])
+// // console.log(mevalar[-4])
+// console.log(mevalar.length)
+
+//! ___________________________________________________________________________
+
+let oquvchilar = ["Boburjon", "Marjona", "Abrorbek", "Hurmatbek", "Shohruzbek", "Dastonbek"]
+let natija = []
+
+oquvchilar.push("Sherzodbek", "Axror")
+
+console.log(oquvchilar)
+
+
+
+
+
+
+
+
+
+// console.log(oquvchilar)
+// console.log(oquvchilar.length)
+// console.log(oquvchilar[0])
+// console.log(oquvchilar[5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
