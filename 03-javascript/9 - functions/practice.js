315062aeef7768d80c2fe734c2ac8aa8319a3aac
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-26 23:58:45 +0500

    .

--- 03-javascript/9 - functions/practice.js
+++ 03-javascript/9 - functions/practice.js
@@ -98,6 +98,7 @@ function masala16(massiv) {
 }
 
 
+
 // console.log(masala16([1, 2, 3, 4, 5]));
 // console.log(masala16([-5, -2, -10]));
 // 17-masala: [array] Massivdagi eng kichik elementni topuvchi funksiya yozing.
