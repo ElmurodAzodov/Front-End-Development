7b42a6e35221281262e041faa0a846914546181f
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-07-28 19:42:55 +0500

    .

--- 03-javascript/9 - functions/practice.js
+++ 03-javascript/9 - functions/practice.js
@@ -110,7 +110,7 @@ function masala17(massiv) {
   }
   return kichik;
 }
-console.log(masala17([4, 3, 2, 5, 6, 7, 8, 1, 0]));
+// console.log(masala17([4, 3, 2, 5, 6, 7, 8, 1, 0]));
 
 // 18-masala: [array] Massiv elementlarini ikkiga ko'paytirib,
 // yangi massiv qaytaruvchi funksiya yozing.
