13d75d637400a630e2198eb4a4512ffb32d93083
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-04 16:05:15 +0500

    .

--- 03-javascript/6 - arrays/practice.js
+++ 03-javascript/6 - arrays/practice.js
@@ -1,6 +1,7 @@
 // Topshiriq metodlar 20ta
 
 let massiv = ["olma", "anor", "o'rik", "anjir", "shaftoli", "apelsin", "mandarin"]
+let massiv1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 // 1. Berilgan massivga oxiriga "JavaScript" so'zini push() orqali qo'shing
 // 2. Berilgan massivning boshidan 2 ta elementni shift() orqali olib tashlang
 // 3. Berilgan massivning 2-indeksidan boshlab 3 ta elementni o'chirib, o'rniga 
