a3ee017cf00cbaa8079a3e56d9d1359c37e66219
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-05 16:37:02 +0500

    add

--- 03-javascript/14 - built-in-objects/topshiriq.js
+++ 03-javascript/14 - built-in-objects/topshiriq.js
@@ -1,11 +1,14 @@
 
 //* 1-topshiriq. Hozirgi sana va vaqtni konsolga chiqaruvchi dastur yozing.
-console.log(new Date())
+const sana = new Date()
+console.log(sana)
 
 //* 2-topshiriq. Hozirgi yilni konsolga chiqaruvchi dastur yozing.
-
+const hozirgiYil = new Date().getFullYear()
+console.log(hozirgiYil)
 
 //* 3-topshiriq. Hozirgi oy raqamini (0-11 oralig'ida) konsolga chiqaruvchi dastur yozing.
+
 
 //* 4-topshiriq. Hozirgi kun raqamini (1-31 oralig'ida) konsolga chiqaruvchi dastur yozing.
 
