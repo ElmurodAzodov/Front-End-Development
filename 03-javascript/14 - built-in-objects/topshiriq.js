b3220da89422d3c93bb0fb5964e50c08e67f0d18
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-05 17:01:07 +0500

    add

--- 03-javascript/14 - built-in-objects/topshiriq.js
+++ 03-javascript/14 - built-in-objects/topshiriq.js
@@ -1,23 +1,29 @@
 
 //* 1-topshiriq. Hozirgi sana va vaqtni konsolga chiqaruvchi dastur yozing.
-const sana = new Date()
-console.log(sana)
+// const sana = new Date()
+// console.log(sana)
 
 //* 2-topshiriq. Hozirgi yilni konsolga chiqaruvchi dastur yozing.
-const hozirgiYil = new Date().getFullYear()
-console.log(hozirgiYil)
+// const hozirgiYil = new Date().getFullYear()
+// console.log(hozirgiYil)
 
 //* 3-topshiriq. Hozirgi oy raqamini (0-11 oralig'ida) konsolga chiqaruvchi dastur yozing.
-const hozirgiOy = new Date().getMonth()
-console.log(hozirgiOy)
+// const hozirgiOy = new Date().getMonth()
+// console.log(hozirgiOy)
 
 //* 4-topshiriq. Hozirgi kun raqamini (1-31 oralig'ida) konsolga chiqaruvchi dastur yozing.
 
 //* 5-topshiriq. Hozirgi sanani ISO formatda (YYYY-MM-DDTHH:mm:ss.sssZ) konsolga chiqaruvchi dastur yozing.
 
 //* 6-topshiriq. 1970-yil 1-yanvardan hozirgi vaqtgacha bo'lgan millisekundlar sonini konsolga chiqaruvchi dastur yozing.
+// const hozirgiVaqt = Date.now()
+// console.log(hozirgiVaqt)
 
 //* 7-topshiriq. Hozirgi sanani o'zbek tilida ko'rsatuvchi dastur yozing. (Intl.DateTimeFormat dan foydalaning)
+const sana = new Date(2026, 7, 3, 14, 30);
+
+const uzFormat = new Intl.DateTimeFormat("uz-UZ");
+console.log(uzFormat.format(sana));
 
 //* 8-topshiriq. Hozirgi vaqtga 5 soat qo'shib, yangi vaqtni konsolga chiqaruvchi dastur yozing.
 
