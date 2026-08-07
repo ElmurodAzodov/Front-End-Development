b16372228639ad08e443c3f495f0f37e5dfde562
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-07 17:51:42 +0500

    add

--- 03-javascript/17 - document-object-model-(DOM)/main.js
+++ 03-javascript/17 - document-object-model-(DOM)/main.js
@@ -1,11 +1,65 @@
-const box = document.getElementById("box");
-const loremTexts = document.getElementsByClassName("lorem")
-const lorem = document.getElementById("lorem")
+// const box = document.getElementById("box");
+// const loremTexts = document.getElementsByClassName("lorem");
+// const lorem = document.getElementById("lorem");
 
+// box.style.backgroundColor = "blue";
+// box.style.width = "500px";
+// box.style.height = "500px";
+// box.style.borderRadius = "50%";
+// lorem.style.color = "red";
+// console.log(loremTexts.length);
 
-box.style.backgroundColor = "blue";
-box.style.width = "500px";
-box.style.height = "500px";
-box.style.borderRadius = "50%"
-lorem.style.color = "red"
-console.log(loremTexts.length)
+//* ======================================================================
+
+const container = document.getElementById("container");
+const box_1 = document.getElementById("box-1");
+const box_2 = document.getElementById("box-2");
+const box_3 = document.getElementById("box-3");
+const box_4 = document.getElementById("box-4");
+const box_5 = document.getElementById("box-5");
+const box_6 = document.getElementById("box-6");
+
+container.style.width = "1800px";
+container.style.height = "700px";
+container.style.border = "1px solid red";
+container.style.margin = "50px auto";
+container.style.display = "flex";
+container.style.justifyContent = "space-around";
+container.style.alignItems = "center";
+container.style.flexWrap = "wrap";
+
+box_1.style.border = "1px solid green";
+box_1.style.backgroundColor = "red";
+
+box_2.style.border = "1px solid magenta";
+box_2.style.backgroundColor = "green";
+
+box_3.style.border = "1px solid yellow";
+box_3.style.backgroundColor = "magenta";
+
+box_4.style.border = "1px solid orange";
+box_4.style.backgroundColor = "yellow";
+
+box_5.style.border = "1px solid pink";
+box_5.style.backgroundColor = "blue";
+
+box_6.style.border = "1px solid blue";
+box_6.style.backgroundColor = "orange";
+
+box_1.style.width = "200px";
+box_1.style.height = "200px";
+
+box_2.style.width = "200px";
+box_2.style.height = "200px";
+
+box_3.style.width = "200px";
+box_3.style.height = "200px";
+
+box_4.style.width = "200px";
+box_4.style.height = "200px";
+
+box_5.style.width = "200px";
+box_5.style.height = "200px";
+
+box_6.style.width = "200px";
+box_6.style.height = "200px";
