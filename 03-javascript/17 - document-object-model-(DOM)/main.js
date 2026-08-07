dfc69dd630e1e5d8502bb71704066dfb8967d1e6
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-07 17:24:40 +0500

    add

---
+++ 03-javascript/17 - document-object-model-(DOM)/main.js
@@ -0,0 +1,11 @@
+const box = document.getElementById("box");
+const loremTexts = document.getElementsByClassName("lorem")
+const lorem = document.getElementById("lorem")
+
+
+box.style.backgroundColor = "blue";
+box.style.width = "500px";
+box.style.height = "500px";
+box.style.borderRadius = "50%"
+lorem.style.color = "red"
+console.log(loremTexts.length)
