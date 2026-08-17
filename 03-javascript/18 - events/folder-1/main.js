0915a88c9975ba023d3b15d1b84c7b4385859264
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-17 17:08:05 +0500

    dark mode and light mode

---
+++ 03-javascript/18 - events/folder-1/main.js
@@ -0,0 +1,15 @@
+
+const btn = document.getElementById("btn")
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark")
+    
+    if (document.body.classList.contains("dark")) {
+        btn.style.fontSize = "46px"
+        btn.textContent = "🌑"
+    } else {
+        btn.style.fontSize = "46px"
+        btn.textContent = "☀️"
+    }
+})
+
