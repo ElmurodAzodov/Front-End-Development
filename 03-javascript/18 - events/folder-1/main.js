f62d708e0775af625d87935f367485f27230ec62
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-17 17:11:10 +0500

    add

--- 03-javascript/18 - events/folder-1/main.js
+++ 03-javascript/18 - events/folder-1/main.js
@@ -1,15 +1,13 @@
-
-const btn = document.getElementById("btn")
+const btn = document.getElementById("btn");
 
 btn.addEventListener("click", () => {
-    document.body.classList.toggle("dark")
-    
-    if (document.body.classList.contains("dark")) {
-        btn.style.fontSize = "46px"
-        btn.textContent = "🌑"
-    } else {
-        btn.style.fontSize = "46px"
-        btn.textContent = "☀️"
-    }
-})
+  document.body.classList.toggle("dark");
 
+  if (document.body.classList.contains("dark")) {
+    btn.style.fontSize = "46px";
+    btn.textContent = "🌑";
+  } else {
+    btn.style.fontSize = "46px";
+    btn.textContent = "☀️";
+  }
+});
