56c1b7e66509ad341754185cbfce5ea4b8dbc76e
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-17 16:33:58 +0500

    add

--- 03-javascript/17 - document-object-model-(DOM)/folder-1/main.js
+++ 03-javascript/17 - document-object-model-(DOM)/folder-1/main.js
@@ -1,7 +1,16 @@
-const btn = document.querySelector("#btn");
-const menu = document.querySelector("#menu");
+// const btn = document.querySelector("#btn");
+// const menu = document.querySelector("#menu");
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-//   console.log(menu.classList.toggle("active"))
-});
+// btn.addEventListener("click", () => {
+//   menu.classList.toggle("active");
+// //   console.log(menu.classList.toggle("active"))
+// });
+
+//* ===============================================
+
+
+const box = document.querySelector("#box");
+const btn = document.querySelector("#btn");
+
+console.log(box.contains(btn));
+console.log(btn.contains(box));
