e100a8808442cfa997997dd5e86cd3fb7df7b169
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-21 14:01:05 +0500

    add

---
+++ 03-javascript/9 - functions/dars/main.js
@@ -0,0 +1,21 @@
+
+//* Function Declaration
+
+function salomMatni() {
+    console.log("Assalomu alaykum, bu function declaration")
+}
+
+salomMatni()
+
+//* Function Expression
+
+const salomMatniChiqar = function () {
+    console.log("Assalomu alaykum, bu function expression")
+}
+
+salomMatniChiqar()
+
+//* Arrow Functions
+const salomMatniChiqaruvchi = () => console.log("Bu arrow functions")
+
+salomMatniChiqaruvchi()
