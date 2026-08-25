c999069ced2322f34c4aa4970cf2866d533069f8
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-08-25 17:58:48 +0500

    add

--- 03-javascript/9 - functions/dars/main.js
+++ 03-javascript/9 - functions/dars/main.js
@@ -28,6 +28,39 @@ function kattaHarfgaOzgartiruvchi(matn) {
     return String(matn).toUpperCase()
 }
 
-console.log(kattaHarfgaOzgartiruvchi("Men dasturchiman"))
-console.log(kattaHarfgaOzgartiruvchi("kiritilgan matnni katta harfga o'zgartirdi"))
+// console.log(kattaHarfgaOzgartiruvchi("Men dasturchiman"))
+// console.log(kattaHarfgaOzgartiruvchi("kiritilgan matnni katta harfga o'zgartirdi"))
 
+
+// function yigindi(a, b) {
+//     console.log(a + b)
+// }
+
+// yigindi(5, 9)
+
+//* a va b sonlar berilgan, ulardan eng kattasini chiqaruvchi funksiya yozing.
+function kattaSon(a, b) {
+    if (a > b) {
+        return `${a} soni katta`
+    } else {
+        return `${b} soni katta`
+    }
+}
+// console.log(kattaSon(15, 11))
+
+//* a va b sonlar berilgan, ulardan qaysilari juft son ekanligini chiqaruvchi funksiya yozing.
+
+// a % 2 === 0
+function juftSon(a, b) {
+    if (a % 2 === 0 && b % 2 !== 0) {
+        return `${a} soni juft son!`
+    } else if(a % 2 === 0 && b % 2 === 0) {
+        return "Siz kiritgan ikkita son ham juft sonlar!"
+    } else if(a % 2 !== 0 && b % 2 === 0) {
+        return `${b} soni juft son!`
+    } else {
+        return "Ikkala son ham juft emas!"
+    }
+}
+
+console.log(juftSon(2, 5))
