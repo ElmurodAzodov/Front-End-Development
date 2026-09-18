eca49eb958de234cd7ef4f5f22b6e4e6920e4101
ElmurodAzodov <elmurodazodov002@gmail.com>
2026-09-18 17:29:20 +0500

    add

--- 03-javascript/18 - events/carusel/main.js
+++ 03-javascript/18 - events/carusel/main.js
@@ -4,3 +4,25 @@ const nextBtn = document.getElementById("nextBtn");
 
 let currentIndex = 0;
 
+const cardWidth = 210;
+
+nextBtn.addEventListener("click", () => {
+  if (currentIndex < 3) {
+    currentIndex++;
+    updateCarousel();
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
+
+function updateCarousel() {
+  // Yo'lakni chapga surish (minus qiymat)
+  const moveAmount = -currentIndex * cardWidth;
+  // CSS transform orqali harakatlantirish
+  track.style.transform = `translateX(${moveAmount}px)`;
+}
